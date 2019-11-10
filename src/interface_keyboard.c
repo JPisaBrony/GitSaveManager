23deb8249aeab83466c0e8fc5c3d9d9b3ff54454
JPisaBrony <jdpohl789@gmail.com>
2019-11-10 18:46:17 -0500

    + shift key implementation and some cleanup

--- src/interface_keyboard.c
+++ src/interface_keyboard.c
@@ -30,6 +30,16 @@ char *keys_upper[KEYS_ROWS] =
  "ZXCVBNM"
 };
 
+void switch_letter_case() {
+    if(key_state == KEYS_LOWER) {
+        keys = keys_upper;
+        key_state = KEYS_UPPER;
+    } else if(key_state == KEYS_UPPER) {
+        keys = keys_lower;
+        key_state = KEYS_LOWER;
+    }
+}
+
 void key_button(char letter) {
     char msg[2];
     msg[0] = letter;
@@ -44,6 +54,10 @@ void key_button(char letter) {
     if(mouse_x > rect.x && mouse_x < rect.x + rect.w && mouse_y > rect.y && mouse_y < rect.y + rect.h && mouse_pressed == 1) {
         SDL_FillRect(screen, &rect, text_color_bg_pressed);
         if(mouse_just_pressed == 1) {
+            if(shift_pressed == 1) {
+                switch_letter_case();
+                shift_pressed = 0;
+            }
             strcat(input, msg);
             mouse_just_pressed = 0;
         }
@@ -57,8 +71,6 @@ void key_button(char letter) {
     SDL_BlitSurface(text, NULL, screen, &rect);
     // free the text to prevent memory leaks
     SDL_FreeSurface(text);
-
-
 }
 
 void special_key_button(char *msg, int action) {
@@ -85,27 +97,15 @@ void special_key_button(char *msg, int action) {
                 break;
             case SPECIAL_KEY_ACTION_CAPS:
                 if(mouse_just_pressed == 1) {
-                    if(key_state == KEYS_LOWER) {
-                        keys = keys_upper;
-                        key_state = KEYS_UPPER;
-                    } else if(key_state == KEYS_UPPER) {
-                        keys = keys_lower;
-                        key_state = KEYS_LOWER;
-                    }
+                    switch_letter_case();
                     mouse_just_pressed = 0;
                 }
                 break;
             case SPECIAL_KEY_ACTION_SHIFT:
                 if(mouse_just_pressed == 1) {
-                    if(key_state == KEYS_LOWER) {
-                        keys = keys_upper;
-                        key_state = KEYS_UPPER;
-                    } else if(key_state == KEYS_UPPER) {
-                        keys = keys_lower;
-                        key_state = KEYS_LOWER;
-                    }
+                    switch_letter_case();
                     mouse_just_pressed = 0;
-                    shift_pressed = 1;
+                    shift_pressed = !shift_pressed;
                 }
                 break;
             default:
@@ -125,19 +125,21 @@ void special_key_button(char *msg, int action) {
 }
 
 void keyboard_init() {
-    // text color
+    // setup text color
     text_color_fg.r = 0xFF;
     text_color_fg.g = 0xFF;
     text_color_fg.b = 0xFF;
     text_color_bg = SDL_MapRGB(screen->format, 0x00, 0x37, 0xFF);
     text_color_bg_pressed = SDL_MapRGB(screen->format, 0x03, 0xF8, 0xFC);
 
+    // setup input
     input = malloc(MAX_INPUT_LENGTH);
     input[0] = '\0';
 
     // setup keys
     keys = keys_lower;
 
+    // testing
     while(1) {
         // check for pending events
         while(SDL_PollEvent(&e)) {
@@ -165,41 +167,42 @@ void keyboard_init() {
         // clear screen
         SDL_FillRect(screen, NULL, 0x00000000);
 
-        // print input to top of screen
-        rect.x = 0;
-        rect.y = 0;
-        // create text with the font and color
-        SDL_Surface *text = TTF_RenderText_Solid(font, input, text_color_fg);
-        // blit the text to the screen
-        SDL_BlitSurface(text, NULL, screen, &rect);
-        // free the text to prevent memory leaks
-        SDL_FreeSurface(text);
-
-        for(i = 0; i < KEYS_ROWS; i++) {
-            rect.y = KEYBOARD_START_Y + (i * KEYBOARD_Y_INCREMENT) + KEYBOARD_KEY_SPACING;
-            int len = strlen(keys[i]);
-            for(j = 0; j < len; j++) {
-                rect.x = KEYBOARD_START_X + (j * KEYBOARD_X_INCREMENT) + KEYBOARD_KEY_SPACING + i * KEYBOARD_KEY_SPACING;
-                key_button(keys[i][j]);
-            }
-        }
-
-        int len = strlen(keys[0]);
-        rect.x = KEYBOARD_START_X + len * KEYBOARD_X_INCREMENT + KEYBOARD_KEY_SPACING;
-        rect.y = KEYBOARD_START_Y + KEYBOARD_KEY_SPACING;
-        special_key_button("<==", SPECIAL_KEY_ACTION_BACKSPACE);
-
-        len = strlen(keys[2]);
-        rect.x = KEYBOARD_START_X + (len + 1) * KEYBOARD_X_INCREMENT + KEYBOARD_KEY_SPACING - KEYBOARD_KEY_BACKGROUND_SPACE;
-        rect.y = KEYBOARD_START_Y + KEYBOARD_Y_INCREMENT * 2 + KEYBOARD_KEY_SPACING;
-        special_key_button("==>", SPECIAL_KEY_ACTION_ENTER);
-
-        //len = strlen(keys[3]);
-        rect.x = KEYBOARD_START_X - KEYBOARD_KEY_BACKGROUND_SPACE;
-        rect.y = KEYBOARD_START_Y + KEYBOARD_Y_INCREMENT * 3 + KEYBOARD_KEY_SPACING;
-        special_key_button("[s]", SPECIAL_KEY_ACTION_SHIFT);
+        show_keyboard();
 
         if(SDL_Flip(screen) == -1)
             exit_msg("Failed to SDL_Flip");
+    }
+}
+
+void show_keyboard() {
+    // print input to top of screen
+    rect.x = 0;
+    rect.y = 0;
+    // create text with the font and color
+    SDL_Surface *text = TTF_RenderText_Solid(font, input, text_color_fg);
+    // blit the text to the screen
+    SDL_BlitSurface(text, NULL, screen, &rect);
+    // free the text to prevent memory leaks
+    SDL_FreeSurface(text);
+
+    // print each key
+    for(i = 0; i < KEYS_ROWS; i++) {
+        rect.y = KEYBOARD_START_Y + (i * KEYBOARD_Y_INCREMENT) + KEYBOARD_KEY_SPACING;
+        int len = strlen(keys[i]);
+        rect.x = KEYBOARD_START_X - KEYBOARD_KEY_BACKGROUND_SPACE;
+
+        if(i == 3)
+            special_key_button("[s]", SPECIAL_KEY_ACTION_SHIFT);
+
+        for(j = 0; j < len; j++) {
+            rect.x = KEYBOARD_START_X + (j * KEYBOARD_X_INCREMENT) + KEYBOARD_KEY_SPACING + i * KEYBOARD_KEY_SPACING;
+            key_button(keys[i][j]);
+        }
+
+        if(i == 1)
+            special_key_button("<==", SPECIAL_KEY_ACTION_BACKSPACE);
+
+        if(i == 2)
+            special_key_button("==>", SPECIAL_KEY_ACTION_ENTER);
     }
 }
